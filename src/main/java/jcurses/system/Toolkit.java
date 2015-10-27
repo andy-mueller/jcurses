084df906498720a06cd4735e4cc38bc1775ee453
andy-mueller <andreas.b.mueller@freenet.de>
2015-10-27 09:36:50 +0100

    -extracted LinePart to its own file

--- src/main/java/jcurses/system/Toolkit.java
+++ src/main/java/jcurses/system/Toolkit.java
@@ -692,35 +692,3 @@ public class Toolkit {
 }
 
 
-class LinePart {
-
-	int _begin = -1;
-	int _end = -2;
-	int _alignment = -1;
-	int _position = -1;
-
-	public LinePart() {
-	}
-
-	LinePart(int begin, int end, int alignment) {
-		_begin = begin;
-		_end = end;
-		_alignment = alignment;
-	}
-
-
-	LinePart(int begin, int end, int position, int alignment) {
-		_begin = begin;
-		_end = end;
-		_position = position;
-		_alignment = alignment;
-	}
-
-	boolean isEmpty() {
-		return (_begin > _end);
-	}
-
-	public String toString() {
-		return "[begin="+_begin+",end="+_end+",align="+_alignment+",isEmpty="+isEmpty();
-	}
-}
